f50d9e6fb2b20a975ac5cfdf814bcae6bf51fa4f
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-12-02 21:20:33 +0600

    Добавьте файлы проекта.

---
+++ PatientCard/Controllers/CardsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PatientCard.Models;
+
+namespace PatientCard.Controllers
+{
+    public class CardsController : Controller
+    {
+        private PatientCardContext db = new PatientCardContext();
+
+        // GET: Cards
+        public ActionResult Index()
+        {
+            return View(db.Cards.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Search(Card patient)
+        {
+            foreach (var p in db.Cards)
+            {
+                if (p.Surname==patient.Surname || p.Iin==patient.Iin)
+                {
+                    return $"Искомая запись в базе имеет идентификационный номер {p.Id}\n" +
+                        $"полное имя пациента: {p.Name} {p.Surname}\n" +
+                        $"ИИН: {p.Iin}";
+                }
+            }
+            return "Указанная вами запись в базе отсутствует.";
+        }
+
+        // GET: Cards/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // GET: Cards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cards/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Surname,Iin,Address,Phone,Specilest,DoctorName,DoctorSurname,Diagnosis,Complain,Date")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cards.Add(card);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(card);
+        }
+
+        // GET: Cards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // POST: Cards/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Surname,Iin,Address,Phone,Specilest,DoctorName,DoctorSurname,Diagnosis,Complain,Date")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(card).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(card);
+        }
+
+        // GET: Cards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // POST: Cards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Card card = db.Cards.Find(id);
+            db.Cards.Remove(card);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PatientCard/Models/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatientCard.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Iin { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Specilest { get; set; }
+        public string DoctorName { get; set; }
+        public string DoctorSurname { get; set; }
+        public string Diagnosis { get; set; }
+        public string Complain { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
