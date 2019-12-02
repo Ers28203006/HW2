b5206ee8eb7009329c0dd45516fba48c27913a55
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-12-02 21:44:02 +0600

    aded initializer

--- PatientCard/Global.asax.cs
+++ PatientCard/Global.asax.cs
@@ -14,6 +14,7 @@ namespace PatientCard
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new CardDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ PatientCard/Models/CardDbInitializer .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PatientCard.Models
+{
+    public class CardDbInitializer : DropCreateDatabaseAlways<PatientCardContext>
+    {
+        protected override void Seed(PatientCardContext db)
+        {
+            db.Cards.Add(new Card { Name="Jimmi", Surname="Jimmi", Iin="850128389394", Phone="87024859459", Address="Astana c, A-98 st, 9 h, 9 fl",
+                                    Complain= "head ache", Diagnosis= "migraine", DoctorName="Andrea", DoctorSurname="Va", Specilest= "therapist", Date=DateTime.Now
+            });
+
+            base.Seed(db);
+        }
+    }
+}
